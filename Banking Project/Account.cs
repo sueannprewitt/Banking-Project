d21674d7d183a3fc1c5a0a159c60f4e286613ac2
Sue Ann <sueannprewitt@yahoo.com>
2017-09-07 16:38:31 -0400

    initial commit

---
+++ Banking Project/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Project {
+	class Account {
+		//Properties go here
+		private double Balance = 0;
+		private string Name = "Checking";
+		private int Number = 0;
+		//Methods go here
+		public void Deposit(double amount) {
+			Balance += amount;
+		}
+		public void Withdraw(double amount) {
+			if(amount > CheckBalance()) {
+				Console.WriteLine("Insufficient funds.");
+			} else {
+				Balance -= amount;
+			}
+			
+		}
+		public double CheckBalance() {
+			return Balance;
+		}
+
+		public string GetName() {
+			return Name;
+		}
+
+		public void SetName(string name) {
+			Name = name;
+		}
+		public int getNumber () {
+			return Number;
+		}
+		private void SetNumber(int number) {
+
+		}
+
+		public Account() {
+
+		}
+
+		public Account(string name) {
+			Name = name;
+		}
+
+		}
+
+}
--- Banking Project/Program.cs
+++ Banking Project/Program.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Banking_Project {
 	class Program {
-		static void Main(string[] args) {
+
+		void Run() {
+			Account checking = new Account();
+			checking.SetName("Checking 1");
+			checking.Withdraw(100.00);
+			checking.Deposit(50.00);
+			checking.Deposit(-100.00);
+			Console.WriteLine($"Account {checking.GetName()}, balance is {checking.CheckBalance()}");
 		}
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+
+		
 	}
 }
