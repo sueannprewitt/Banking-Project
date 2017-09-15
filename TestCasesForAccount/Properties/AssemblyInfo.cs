173baa1eb8968ca5d016bbb27ba780991fcf7c03
Sue Ann <sueannprewitt@yahoo.com>
2017-09-15 13:49:59 -0400

    Test case project for Accounting class added

---
+++ TestCasesForAccount/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCasesForAccount")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCasesForAccount")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2ea287ed-ac42-4724-8b92-e11bfda3ad9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestCasesForAccount/TestsForAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankingLibrary;
+
+namespace TestCasesForAccount
+{
+    [TestClass]
+    public class TestsForAccount
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
