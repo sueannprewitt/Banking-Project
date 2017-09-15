e9c81b169c1cf3d46e01caa5d577206109fa9259
Sue Ann <sueannprewitt@yahoo.com>
2017-09-15 14:01:50 -0400

    More testing

--- TestCasesForAccount/TestsForAccount.cs
+++ TestCasesForAccount/TestsForAccount.cs
@@ -8,8 +8,28 @@ namespace TestCasesForAccount
     public class TestsForAccount
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Testing()
         {
+            Account checking = new Account();
+            Assert.IsInstanceOfType(checking, typeof(Account), "Default Constructor");
+
+            Assert.AreEqual(96434, checking.getNumber(), "Account Number is 96434");
+
+
+                }
+
+
+        [TestMethod]
+        public void TestDepositinChecking()
+            {
+            Account checking = new Account();
+            checking.Deposit(100.00);
+            var balance = checking.CheckBalance();
+            Assert.AreEqual(balance, checking.CheckBalance());
+       
+
         }
     }
 }
+
+
